b77a731093cabcdea075bac9962ea8115fe1f358
Cristian Tafur <cdtafurd@unal.edu.co>
2019-05-03 01:07:01 +0000

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    #get 'helloworld', to: 'welcome#index'
+    
+    #namespace :admin do #para prefijo de admin
+        #resources :authors, only: [:create,:show]  #solo creeme las rutas apra teales acciones
+        
+    #end
+    
+    #namespace :admin do
+       #get 'statistics', to: 'stats#index' 
+    #end
+    
+    resources :authors do
+       resources :books 
+    end
 end
